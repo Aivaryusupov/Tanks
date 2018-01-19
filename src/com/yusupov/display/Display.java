adc96c23e6d7d081060b3a2966f6fa0cd4777f8d
aivaryusupov <aivaryusupov@gmail.com>
2018-01-19 16:33:22 +0300

    add lesson 5, multibuffering and antialiasing

--- src/com/yusupov/display/Display.java
+++ src/com/yusupov/display/Display.java
@@ -2,6 +2,7 @@ package com.yusupov.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Arrays;
@@ -24,9 +25,11 @@ public abstract class Display {
     private static Graphics bufferGraphics;
     private static int clearColor;
 
+    private static BufferStrategy bufferStrategy;
+
     private static float delta = 0;
 
-    public static void create(int width, int height, String title, int _clearColor) {
+    public static void create(int width, int height, String title, int _clearColor, int numBuffers) {
 
         if (created)
             return;
@@ -49,6 +52,9 @@ public abstract class Display {
         bufferGraphics = buffer.getGraphics();
         clearColor = _clearColor;
 
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+
         created = true;
 
     }
@@ -60,12 +66,18 @@ public abstract class Display {
     public static void render() {
         bufferGraphics.setColor(new Color(0xff0000ff));
         bufferGraphics.fillOval((int)(350 + (Math.sin(delta) * 200)), 250, 100, 100);
+
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        bufferGraphics.fillOval((int)(500 + (Math.sin(delta) * 200)), 250, 100, 100);
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
         delta += 0.02f;
     }
 
     public static void swapBuffers() {
-        Graphics g = content.getGraphics();
+        Graphics g = bufferStrategy.getDrawGraphics();
         g.drawImage(buffer, 0, 0, null);
+        bufferStrategy.show();
     }
 
 }
